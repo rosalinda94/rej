29ca2d4cd11eeffe805dad47924e10cc4b7773b6
Emilia Segura <emiliasegura@Emilias-MacBook-Pro.local>
2019-05-14 21:57:17 -0300

    con login

--- login.php
+++ login.php
@@ -1,5 +1,28 @@
 <?php
 include_once 'header.php';
+require 'funciones.php';
+
+$emails='emilia_segura@hotmail.com';
+$passwords=123;
+$errors=[];
+
+if (!empty($_POST)) {
+    if (isset ($_POST['email'])) {
+      if ($_POST['email']!=$emails) {
+        $errors[]= 'Error de email';
+      }
+    }
+    if (isset($_POST['password'])) {
+      if ($_POST['password']!=$passwords) {
+        $errors[]= 'Error de password';
+      }
+    }
+    if (empty($errors)) {
+      redirect('home.php');
+    }
+}
+
+
  ?>
 
 <!DOCTYPE html>
@@ -13,17 +36,22 @@ include_once 'header.php';
       <article id="login">
     		<h2>Te damos la bienvenida a Buddy</h2>
         <p class="condiciones">Encontrá a tu mascota ideal</p>
-      <form>
+      <form class="" action="login.php" method="post">
         <div class="form-group">
-          <input type="text" class="form-control" id="email" placeholder="Email">
+          <input type="text" class="form-control" id="email" name="email" placeholder="Email">
         </div>
         <div class="form-group">
-          <input type="password" class="form-control" id="password" placeholder="Contraseña">
+          <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña">
         </div>
-        <input type="button" name="boton" value="Iniciar Sesion" id="boton"><br>
+        <div class="">
+          <?php foreach ($errors as $error) {
+            echo $error;
+            echo '<br>';
+          } ?>
+        </div>
+        <input type="submit" name="boton" value="Iniciar Sesion" id="boton"><br>
         <p class="condiciones"> o</p>
-        <input type="button" name="boton" value="Continuar con facebook" id="boton-face">
-        <article class="condiciones">
+
           <p> <br>Al continuar aceptas las <b>Condiciones del servicio </b> y la
             <b>Politica de la privacidad </b> de Buddy </p>
           <p><b>¿Olvidaste tu contraseña?</b></p>
